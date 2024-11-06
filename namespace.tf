1aef987bca374ce4906604cd49ce362a6b6d850b
Martin Sarrionandia <martin@sarrionandia.co.uk>
2024-11-06 13:58:35 +0000

    Fix spellin' mistake

--- namespace.tf
+++ namespace.tf
@@ -2,7 +2,7 @@ resource "kubernetes_namespace" "this" {
   metadata {
     name = "${var.release-name}-${var.release-chart}"
     labels = {
-      "pod-security.kubernetes.io/enforce" =  var.initial-setup ? "priviledged" : "baseline"
+      "pod-security.kubernetes.io/enforce" =  var.initial-setup ? "privileged" : "baseline"
       "pod-security.kubernetes.io/audit"   = "baseline"
       "pod-security.kubernetes.io/warn"    = "baseline"
     }
