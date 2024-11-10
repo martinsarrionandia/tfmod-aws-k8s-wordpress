ff48aceb73d8d219833665eb8285b8ef1956a680
Martin Sarrionandia <martin@sarrionandia.co.uk>
2024-11-10 22:48:24 +0000

    Fix var.wordpress-uploads-dir

--- local.tf
+++ local.tf
@@ -5,7 +5,7 @@ locals {
   )
 
   s3-region                      = data.aws_s3_bucket.cdn_bucket.region
-  s3-cdn-wordpresss-uploads-path = "${var.cdn-bucket-name}/${wordpress-uploads-dir}"
+  s3-cdn-wordpresss-uploads-path = "${var.cdn-bucket-name}/${var.wordpress-uploads-dir}"
   c1                             = "c${substr(parseint(sha256(var.release-name), 16), 0, 3)}"
   c2                             = "c${substr(parseint(sha256(var.release-chart), 16), 0, 3)}"
   selinux-level                  = "s0:${local.c1},${local.c2}"
