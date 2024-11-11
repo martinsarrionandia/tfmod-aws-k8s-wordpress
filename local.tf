f9913e13b75bb57521c003481f4419364527467a
Martin Sarrionandia <martin@sarrionandia.co.uk>
2024-11-11 20:32:57 +0000

    toString

--- local.tf
+++ local.tf
@@ -14,7 +14,7 @@ locals {
   }
 
   middleware_rewrite_cdn = "${kubernetes_namespace.this.metadata.0.name}-${local.middleware-cdn-rewrite-name}@kubernetescrd"
-  middlewares = [local.middleware_rewrite_cdn, [for m in var.additional-middlewares: m.value]]
+  middlewares = tostring([local.middleware_rewrite_cdn, [for m in var.additional-middlewares: m.value]])
 
   wordpress-helm-values = <<EOF
 containerSecurityContext:
