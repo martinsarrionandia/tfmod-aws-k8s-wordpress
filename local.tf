4946c3d6076b7cc89de6aee9baf1cc747d655c7c
Martin Sarrionandia <martin@sarrionandia.co.uk>
2024-11-11 20:42:45 +0000

    ,,,,, Chameleons

--- local.tf
+++ local.tf
@@ -14,8 +14,7 @@ locals {
   }
 
   middleware_rewrite_cdn = "${kubernetes_namespace.this.metadata.0.name}-${local.middleware-cdn-rewrite-name}@kubernetescrd"
-  middlewares = [ local.middleware_rewrite_cdn,
-                [for m in var.additional-middlewares: m.value] ]
+  middlewares = "${local.middleware_rewrite_cdn}, ${join(", ",  var.additional-middlewares)}"
 
   wordpress-helm-values = <<EOF
 containerSecurityContext:
