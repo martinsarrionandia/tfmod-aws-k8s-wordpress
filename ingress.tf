5f305f287aafe46807f414edcdc08c2acf6076f4
Martin Sarrionandia <martin@sarrionandia.co.uk>
2024-11-17 22:48:44 +0000

    Change namespace for IngressRoute

--- ingress.tf
+++ ingress.tf
@@ -8,6 +8,48 @@ ingress-route-manifest = <<EOF
 apiVersion: traefik.containo.us/v1alpha1
 kind: IngressRoute
 metadata:
+  name: wordpress
+  namespace: kube-system
+  annotations:
+    cert-manager.io/cluster-issuer: "${var.cluster-issuer}"
+    external-dns.alpha.kubernetes.io/hostname: "${local.fqdn}"
+    external-dns.alpha.kubernetes.io/target: "${var.public-ip}"
+spec:
+  entryPoints:
+    - http
+  #  - websecure
+  routes:
+  - kind: Rule
+    match: Host(${local.fqdn}) && PathPrefix(`/`)
+    priority: 10
+    #middlewares:
+    #- name: "${local.middleware-cdn-rewrite-name}"
+    #  namespace: "${kubernetes_namespace.this.metadata.0.name}"
+    services:
+    - kind: Service
+      name: "${helm_release.wordpress.metadata.0.name}-${helm_release.wordpress.metadata.0.chart}"
+      namespace: "${kubernetes_namespace.this.metadata.0.name}"
+      passHostHeader: true
+      port: 80 
+      responseForwarding:
+        flushInterval: 1ms
+      scheme: https
+      serversTransport: transport
+  tls:
+    secretName: "${var.cluster-issuer}"
+    #options:
+    #  name: 
+    #  namespace: default
+    #certResolver: "${var.cluster-issuer}"
+    domains:
+    - main: "${local.fqdn}"
+
+EOF
+
+ingress-route-manifest2 = <<EOF
+apiVersion: traefik.containo.us/v1alpha1
+kind: IngressRoute
+metadata:
   name: "${helm_release.wordpress.metadata.0.name}"
   namespace: "${kubernetes_namespace.this.metadata.0.name}"
   annotations:
@@ -20,7 +62,7 @@ spec:
   #  - websecure
   routes:
   - kind: Rule
-    match: Host(`${local.fqdn}`) && PathPrefix(`/`)
+    match: Host(${local.fqdn}) && PathPrefix(`/`)
     priority: 10
     #middlewares:
     #- name: "${local.middleware-cdn-rewrite-name}"
