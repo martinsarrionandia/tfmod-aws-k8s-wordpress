a6e41724cf30457dab948ababd59fc3cb8709db4
Martin Sarrionandia <martin@sarrionandia.co.uk>
2025-03-30 21:32:58 +0100

    feat: Add redirect middleware to list

--- locals.tf
+++ locals.tf
@@ -9,10 +9,11 @@ locals {
   c1                              = "c${substr(parseint(sha256(var.release-name), 16), 0, 3)}"
   c2                              = "c${substr(parseint(sha256(var.release-chart), 16), 0, 3)}"
   selinux-level                   = "s0:${local.c1},${local.c2}"
-  middleware-schema-redirect-name = "${var.release-name}-schema-redirect"
+  middleware-scheme-redirect-name = "${var.release-name}-scheme-redirect"
+  middleware-scheme-redirect      = "${kubernetes_namespace.this.metadata[0].name}-${local.middleware-scheme-redirect-name}@kubernetescrd"
   middleware-cdn-rewrite-name     = "${var.release-name}-cdn-rewrite"
   middleware-cdn-rewrite          = "${kubernetes_namespace.this.metadata[0].name}-${local.middleware-cdn-rewrite-name}@kubernetescrd"
-  middlewares                     = join(", ", concat([local.middleware-cdn-rewrite], var.additional-middlewares))
+  middlewares                     = join(", ", concat([local.middleware-scheme-redirect], [local.middleware-cdn-rewrite], var.additional-middlewares))
   aws_access_key_id               = jsondecode(data.aws_secretsmanager_secret_version.s3_access_current.secret_string)["aws_access_key_id"]
   aws_secret_access_key           = jsondecode(data.aws_secretsmanager_secret_version.s3_access_current.secret_string)["aws_secret_access_key"]
 }
