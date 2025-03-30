e0fbbf7e939ce30f99e3c51f878088183b1220bf
Martin Sarrionandia <martin@sarrionandia.co.uk>
2025-03-30 22:19:16 +0100

    fix: naming

--- locals.tf
+++ locals.tf
@@ -9,7 +9,7 @@ locals {
   c1                              = "c${substr(parseint(sha256(var.release-name), 16), 0, 3)}"
   c2                              = "c${substr(parseint(sha256(var.release-chart), 16), 0, 3)}"
   selinux-level                   = "s0:${local.c1},${local.c2}"
-  middleware-scheme-redirect-name = "${var.release-name}-scheme-redirect"
+  middleware-scheme-redirect-name = "${var.release-name}-redirect-scheme"
   middleware-scheme-redirect      = "${kubernetes_namespace.this.metadata[0].name}-${local.middleware-scheme-redirect-name}@kubernetescrd"
   middleware-cdn-rewrite-name     = "${var.release-name}-cdn-rewrite"
   middleware-cdn-rewrite          = "${kubernetes_namespace.this.metadata[0].name}-${local.middleware-cdn-rewrite-name}@kubernetescrd"
