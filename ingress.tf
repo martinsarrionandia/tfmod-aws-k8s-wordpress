d80aa0b46e3ebeaaf02f31a610574c5ed1ada050
Martin Sarrionandia <martin@sarrionandia.co.uk>
2026-01-23 14:59:58 +0000

    feat: separate wordpress, wp-admin and ajax ingress paths

--- ingress.tf
+++ ingress.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_manifest" "this_ingress" {
+resource "kubernetes_manifest" "this_ingress_wordpress" {
   manifest = {
     apiVersion = "networking.k8s.io/v1"
     kind       = "Ingress"
@@ -9,11 +9,12 @@ resource "kubernetes_manifest" "this_ingress" {
         "external-dns.alpha.kubernetes.io/target"          = var.public_ip
         "cert-manager.io/cluster_issuer"                   = var.cluster_issuer
         "traefik.ingress.kubernetes.io/router.middlewares" = local.middlewares
+        "traefik.ingress.kubernetes.io/router.priority"    = "10"
       }
       labels = {
         app = var.release_chart
       }
-      name      = var.release_name
+      name      = "${var.release_name}-wordpress"
       namespace = kubernetes_namespace_v1.this.metadata[0].name
     }
     spec = {
@@ -33,6 +34,107 @@ resource "kubernetes_manifest" "this_ingress" {
                 }
                 path     = "/"
                 pathType = "Prefix"
+              },
+            ]
+          }
+        },
+      ]
+      tls = [
+        {
+          hosts = [
+            local.fqdn,
+          ]
+          secretName = var.cluster_issuer
+        },
+      ]
+    }
+  }
+}
+
+
+resource "kubernetes_manifest" "this_ingress_wpadmin" {
+  manifest = {
+    apiVersion = "networking.k8s.io/v1"
+    kind       = "Ingress"
+    metadata = {
+      annotations = {
+        "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
+        "cert-manager.io/cluster_issuer"                   = var.cluster_issuer
+        "traefik.ingress.kubernetes.io/router.priority"    = "20"
+      }
+      labels = {
+        app = var.release_chart
+      }
+      name      = "${var.release_name}-wpadmin"
+      namespace = kubernetes_namespace_v1.this.metadata[0].name
+    }
+    spec = {
+      rules = [
+        {
+          host = local.fqdn
+          http = {
+            paths = [
+              {
+                backend = {
+                  service = {
+                    name = "${var.release_name}-${var.release_chart}"
+                    port = {
+                      number = 80
+                    }
+                  }
+                }
+                path     = "/wp-admin/"
+                pathType = "Prefix"
+              },
+            ]
+          }
+        },
+      ]
+      tls = [
+        {
+          hosts = [
+            local.fqdn,
+          ]
+          secretName = var.cluster_issuer
+        },
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "this_ingress_ajax" {
+  manifest = {
+    apiVersion = "networking.k8s.io/v1"
+    kind       = "Ingress"
+    metadata = {
+      annotations = {
+        "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
+        "cert-manager.io/cluster_issuer"                   = var.cluster_issuer
+        "traefik.ingress.kubernetes.io/router.priority"    = "30"
+      }
+      labels = {
+        app = var.release_chart
+      }
+      name      = "${var.release_name}-ajax"
+      namespace = kubernetes_namespace_v1.this.metadata[0].name
+    }
+    spec = {
+      rules = [
+        {
+          host = local.fqdn
+          http = {
+            paths = [
+              {
+                backend = {
+                  service = {
+                    name = "${var.release_name}-${var.release_chart}"
+                    port = {
+                      number = 80
+                    }
+                  }
+                }
+                path     = "/wp-admin/admin-ajax.php"
+                pathType = "Exact"
               },
             ]
           }
